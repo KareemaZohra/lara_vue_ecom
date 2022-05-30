45b7467c1133bd3bafd62811c831a43538475296
Kareema Zohra <KareemaZohra@users.noreply.github.com>
2022-05-30 14:21:00 +0600

    models, migrations and seeder created fro product

---
+++ database/factories/ProductModelFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Product_model;
+use Faker\Generator as Faker;
+
+$factory->define(Product_model::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/VariantsModelFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Variants_model;
+use Faker\Generator as Faker;
+
+$factory->define(Variants_model::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
