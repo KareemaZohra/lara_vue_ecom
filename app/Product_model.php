2042c0d9b96dca6e701f8a261c3adcedb8595d12
Kareema Zohra <KareemaZohra@users.noreply.github.com>
2022-06-27 20:28:46 +0600

    products seeder added

--- app/Product_model.php
+++ app/Product_model.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Product_model extends Model
 {
     //
--- database/seeds/ProductModelSeeder.php
+++ database/seeds/ProductModelSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Product_model;
+use Faker\Factory as Faker;
 
 class ProductModelSeeder extends Seeder
 {
@@ -11,6 +13,15 @@ class ProductModelSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker::create();
+        $faker->addProvider(new \RauweBieten\PhpFakerClothing\Clothing($faker));
+        for ($i=1; $i<=2; $i++){
+            $product = new Product_model;
+            $product->name = $faker->clothing();
+            $product->price = $faker->numberBetween(10,100);
+            $product->available = $faker->boolean;
+            $product->save();
+        }
+
     }
 }
